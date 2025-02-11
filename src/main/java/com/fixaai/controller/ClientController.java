9d71b3db6841209b7af6abe3cebe88b9a5b9ad22
Filipe Janguez <filipe.janguez@gmail.com>
2025-02-11 15:45:56 -0300

    Creating and adding annotations on controllers

--- src/main/java/com/fixaai/controller/ClientController.java
+++ src/main/java/com/fixaai/controller/ClientController.java
@@ -1,9 +1,11 @@
 package com.fixaai.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
-public class AuthenticationController {
+@RequestMapping("clients")
+@SecurityRequirement(name = "bearer-key")
+public class ClientController {
 }
