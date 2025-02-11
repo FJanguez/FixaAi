86677edfb229b7e3e4e91701482c732f3f9cb65e
Filipe Janguez <filipe.janguez@gmail.com>
2025-02-10 21:13:33 -0300

    Merging local project with remote repository

---
+++ src/test/java/com/fixaai/FixaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
